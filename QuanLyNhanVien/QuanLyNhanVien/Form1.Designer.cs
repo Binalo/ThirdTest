c1fbf6b129c9708f42389665a1b5db1a76298152
Dien <dien25888@gmail.com>
2020-03-15 16:32:59 +0700

    TestVisual

---
+++ QuanLyNhanVien/QuanLyNhanVien/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace QuanLyNhanVien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDeptTel = new System.Windows.Forms.Label();
+            this.txtEmpId = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtDeptName = new System.Windows.Forms.TextBox();
+            this.txtDeptTel = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dgvInfor = new System.Windows.Forms.DataGridView();
+            this.empId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deptName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deptTel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp. ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Emp. Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(370, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dept. Name";
+            // 
+            // lblDeptTel
+            // 
+            this.lblDeptTel.AutoSize = true;
+            this.lblDeptTel.Location = new System.Drawing.Point(383, 96);
+            this.lblDeptTel.Name = "lblDeptTel";
+            this.lblDeptTel.Size = new System.Drawing.Size(51, 13);
+            this.lblDeptTel.TabIndex = 0;
+            this.lblDeptTel.Text = "Dept. Tel";
+            // 
+            // txtEmpId
+            // 
+            this.txtEmpId.Location = new System.Drawing.Point(126, 22);
+            this.txtEmpId.Name = "txtEmpId";
+            this.txtEmpId.Size = new System.Drawing.Size(221, 20);
+            this.txtEmpId.TabIndex = 1;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(126, 59);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(221, 20);
+            this.txtSalary.TabIndex = 1;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(440, 22);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(221, 20);
+            this.txtEmpName.TabIndex = 1;
+            // 
+            // txtDeptName
+            // 
+            this.txtDeptName.Location = new System.Drawing.Point(440, 56);
+            this.txtDeptName.Name = "txtDeptName";
+            this.txtDeptName.Size = new System.Drawing.Size(221, 20);
+            this.txtDeptName.TabIndex = 1;
+            // 
+            // txtDeptTel
+            // 
+            this.txtDeptTel.Location = new System.Drawing.Point(440, 93);
+            this.txtDeptTel.Name = "txtDeptTel";
+            this.txtDeptTel.Size = new System.Drawing.Size(221, 20);
+            this.txtDeptTel.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(704, 20);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(704, 57);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(704, 96);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(704, 136);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dgvInfor
+            // 
+            this.dgvInfor.AllowUserToAddRows = false;
+            this.dgvInfor.AllowUserToDeleteRows = false;
+            this.dgvInfor.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvInfor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.empId,
+            this.empName,
+            this.salary,
+            this.deptName,
+            this.deptTel});
+            this.dgvInfor.Location = new System.Drawing.Point(66, 180);
+            this.dgvInfor.Name = "dgvInfor";
+            this.dgvInfor.ReadOnly = true;
+            this.dgvInfor.Size = new System.Drawing.Size(713, 247);
+            this.dgvInfor.TabIndex = 3;
+            this.dgvInfor.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.onCellClicked);
+            // 
+            // empId
+            // 
+            this.empId.HeaderText = "Emp. ID";
+            this.empId.Name = "empId";
+            this.empId.ReadOnly = true;
+            this.empId.Width = 120;
+            // 
+            // empName
+            // 
+            this.empName.HeaderText = "Emp. Name";
+            this.empName.Name = "empName";
+            this.empName.ReadOnly = true;
+            this.empName.Width = 150;
+            // 
+            // salary
+            // 
+            this.salary.HeaderText = "Salary";
+            this.salary.Name = "salary";
+            this.salary.ReadOnly = true;
+            // 
+            // deptName
+            // 
+            this.deptName.HeaderText = "Dept. Name";
+            this.deptName.Name = "deptName";
+            this.deptName.ReadOnly = true;
+            this.deptName.Width = 150;
+            // 
+            // deptTel
+            // 
+            this.deptTel.HeaderText = "Dept. Tel";
+            this.deptTel.Name = "deptTel";
+            this.deptTel.ReadOnly = true;
+            this.deptTel.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 458);
+            this.Controls.Add(this.dgvInfor);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDeptTel);
+            this.Controls.Add(this.txtDeptName);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmpId);
+            this.Controls.Add(this.lblDeptTel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDeptTel;
+        private System.Windows.Forms.TextBox txtEmpId;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtDeptName;
+        private System.Windows.Forms.TextBox txtDeptTel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dgvInfor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salary;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deptName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deptTel;
+    }
+}
+
---
+++ QuanLyNhanVien/QuanLyNhanVien/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace QuanLyNhanVien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            show();
+        }
+
+        public void show()
+        {
+            XmlHandler handler = new XmlHandler(); //Tao moi doi tuong XmlHandler
+            XmlDocument doc = new XmlDocument(); //Tao moi doi tuong XmlDocument
+            string filename = @"C:\Users\ADMIN\Desktop\Train C#\QuanLyNhanVien\QuanLyNhanVien\QuanLyNhanVien\congty.xml";
+            //Duong dan toi file
+            List<Employee> empList = new List<Employee>();
+            //Tao moi 1 danh sach luu tru cac nhan vien
+            handler.loadDataFromDoc(doc, filename, empList);
+            //lay du lieu cac nhan vien tu file xml them vao danh sach nhan vien
+            dgvInfor.Rows.Clear(); //xoa het du lieu cac dong
+            int rowIndex = 0; //chi so cua dong cua bang
+            foreach (Employee emp in empList)
+            {
+                dgvInfor.Rows.Add(); //Them mot dong moi
+                //chen du lieu vao bang
+                dgvInfor.Rows[rowIndex].Cells[0].Value = emp.EmpID;
+                dgvInfor.Rows[rowIndex].Cells[1].Value = emp.EmpName;
+                dgvInfor.Rows[rowIndex].Cells[2].Value = emp.Salary;
+                dgvInfor.Rows[rowIndex].Cells[3].Value = emp.DeptName;
+                dgvInfor.Rows[rowIndex].Cells[4].Value = emp.DeptTel;
+
+                rowIndex++;//Tang chi so cua cot len
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            XmlHandler handler = new XmlHandler();
+            //Khai bao cac bien luu tru thong tin nhan vien do nguoi dung nhap
+            string empId, empName, deptName, deptTel;
+            float salary;
+            try
+            {
+                //Lay ra cac gia tri
+                empId = txtEmpId.Text;
+                empName = txtEmpName.Text;
+                salary = float.Parse(txtSalary.Text);
+                deptName = txtDeptName.Text;
+                deptTel = txtDeptTel.Text;
+                //Tao moi nhan vien co cac thong tin o tren
+                if (!notEmptyFields()) //neu chua nhap day du thong tin
+                    MessageBox.Show("Any fields must not be empty", "Waring", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    Employee emp = new Employee(empId, empName, salary, deptName, deptTel);
+                    if (!handler.add(emp)) //Neu them khong thanh cong
+                        MessageBox.Show("Can't not add duplicate Emp. ID: " + empId, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else //Thanh cong
+                    {
+                        MessageBox.Show("Sucessfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        show(); //load lai table
+                        clearTextBox(); //xoa noi dung tren text box
+                        txtEmpId.Focus(); //set focus vao o text box EmpId
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private bool notEmptyFields()
+        {
+            if (txtEmpId.Text.Trim() == "" || txtEmpName.Text.Trim() == "")
+                return false;
+            if (txtSalary.Text.Trim() == "" || txtDeptName.Text.Trim() == "")
+                return false;
+            if (lblDeptTel.Text.Trim() == "")
+                return false;
+            return true;
+        }
+
+        public void clearTextBox()
+        {
+            txtEmpId.Text = "";
+            txtEmpName.Text = "";
+            txtSalary.Text = "";
+            txtDeptName.Text = "";
+            txtDeptTel.Text = "";
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            XmlHandler handler = new XmlHandler();
+            //Khai bao cac bien luo tru thong tin nhan vien do nguoi dung nhap
+            string empId, empName, deptName, deptTel;
+            float salary;
+            try
+            {
+                //Lay ra cac gia tri
+                empId = txtEmpId.Text;
+                empName = txtEmpName.Text;
+                salary = float.Parse(txtSalary.Text);
+                deptName = txtDeptName.Text;
+                deptTel = txtDeptTel.Text;
+                //Tao moi nhan vien co cac thong tin o tren
+                if (!notEmptyFields()) //neu chua nhap day du thong tin
+                    MessageBox.Show("Any fields must not be empty", "Waring", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    Employee emp = new Employee(empId, empName, salary, deptName, deptTel);
+                    if (!handler.edit(emp)) //Neu sua khong thanh cong
+                        MessageBox.Show("Emp. ID: " + empId + " is not exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else //Thanh cong
+                    {
+                        MessageBox.Show("Sucessfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        show(); //load lai table
+                        clearTextBox(); //xoa noi dung tren text box
+                        txtEmpId.Focus(); //set focus vao o text box EmpId
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void onCellClicked(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowSeleted = dgvInfor.CurrentCell.RowIndex; //Lay ra dong hien tai dang duoc chon
+            //hien thi cac thong tin o dong hien tai len o text box tuong ung
+            txtEmpId.Text = (string)dgvInfor.Rows[rowSeleted].Cells[0].Value;
+            txtEmpName.Text = (string)dgvInfor.Rows[rowSeleted].Cells[1].Value;
+            float salary = (float)dgvInfor.Rows[rowSeleted].Cells[2].Value;
+            txtSalary.Text = salary.ToString();
+            txtDeptName.Text = (string)dgvInfor.Rows[rowSeleted].Cells[3].Value;
+            txtDeptTel.Text = (string)dgvInfor.Rows[rowSeleted].Cells[4].Value;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string empId = txtEmpId.Text;
+            if (empId.Trim() == "")
+                MessageBox.Show("EmpId must not be empty", "Waring", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                XmlHandler handler = new XmlHandler();
+                if (!handler.delete(empId)) //Neu xoa khong thanh cong
+                    MessageBox.Show("Emp. ID: " + empId + " is not exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else //Thanh cong
+                {
+                    MessageBox.Show("Sucessfully", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    show(); //load lai table
+                    clearTextBox(); //xoa noi dung tren text box
+                    txtEmpId.Focus(); //set focus vao o text box EmpId
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
